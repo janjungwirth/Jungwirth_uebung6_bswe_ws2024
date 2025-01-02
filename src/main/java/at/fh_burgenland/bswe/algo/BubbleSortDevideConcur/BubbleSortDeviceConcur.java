bd0cce7b460260e8732e57f4155c4c73da4734b6
Jan Jungwirth <125290988+janjungwirth@users.noreply.github.com>
2025-01-02 19:03:07 +0100

    BubbleSortDeviceConcur documentation

--- src/main/java/at/fh_burgenland/bswe/algo/BubbleSortDevideConcur/BubbleSortDeviceConcur.java
+++ src/main/java/at/fh_burgenland/bswe/algo/BubbleSortDevideConcur/BubbleSortDeviceConcur.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class BubbleSortDeviceConcur <T extends Comparable<T>>{
 
+    /**
+     * Sorts the given list of elements using a bubble sort algorithm implemented in a concurrent sublist manner.
+     *
+     * @param items the list of elements to be sorted; if null or empty, an empty list is returned
+     * @return a sorted list of elements in ascending order
+     */
     public List<T> sort(List<T> items) {
         if(items==null || items.isEmpty())
             return new LinkedList<>();
@@ -22,12 +28,22 @@ public class BubbleSortDeviceConcur <T extends Comparable<T>>{
         return parts.getFirst();
     }
 
+    /**
+     * Bubble Sort both parts.
+     * This funktion is used to bubble the sub Lists.
+     * @param parts the parts
+     */
     private void bubbleSubLists(LinkedList<LinkedList<T>> parts) {
         BubbleSortDeviceConcur<T> bs = new BubbleSortDeviceConcur<>();
         parts.set(0, bs.bubbleSort(parts.getFirst()));
         parts.set(1, bs.bubbleSort(parts.getLast()));
     }
 
+    /**
+     * Part List in Parts.
+     * @param items the initial List
+     * @return Parted List.
+     */
     private LinkedList<LinkedList<T>> partIntoLists(final List<T> items){
         LinkedList<LinkedList<T>> chunks = new LinkedList<>();
         chunks.add(new LinkedList<>(items.subList(0, items.size()/2)));
@@ -35,6 +51,12 @@ public class BubbleSortDeviceConcur <T extends Comparable<T>>{
         return chunks;
     }
 
+    /**
+     * The Core of the Algorithm.
+     * This function bubbles the List in the correct order.
+     * @param items the to be sorted List.
+     * @return a sorted List.
+     */
     private LinkedList<T> bubbleSort(final List<T> items){
         LinkedList<T> itemsCopy = new LinkedList<>(items);
         for(int y = 0; y < items.size(); y++){
@@ -50,6 +72,13 @@ public class BubbleSortDeviceConcur <T extends Comparable<T>>{
         return itemsCopy;
     }
 
+    /**
+     * Swaps two adjacent elements in a list.
+     *
+     * @param <T> the type of elements in the list, which must be comparable
+     * @param items the list containing the elements to be swapped
+     * @param i the index of the first element to be swapped with its adjacent element
+     */
     private static <T extends Comparable<T>> void swap(List<T> items, int i) {
         T temp = items.get(i);
         items.set(i, items.get(i +1));
--- src/test/java/at/fh_burgenland/bswe/algo/BubbleSortDevideConcur/BubbleSortDeviceConcurTest.java
+++ src/test/java/at/fh_burgenland/bswe/algo/BubbleSortDevideConcur/BubbleSortDeviceConcurTest.java
@@ -11,6 +11,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BubbleSortDeviceConcurTest {
     BubbleSortDeviceConcur<Integer> bs = new BubbleSortDeviceConcur<>();
 
+    /**
+     * Given: An empty List.
+     * When: Array is getting sorted.
+     * Then: The result is an empty List.
+     */
     @Test
     @DisplayName("Test {} Expect {}")
     public void testEmptyList(){
@@ -22,6 +27,11 @@ class BubbleSortDeviceConcurTest {
         assertEquals(testList, expectedList);
     }
 
+    /**
+     * Given: An null List.
+     * When: Array is getting sorted.
+     * Then: The result is an empty List.
+     */
     @Test
     @DisplayName("Test null Expect {}")
     public void testNullList(){
@@ -33,6 +43,11 @@ class BubbleSortDeviceConcurTest {
         assertEquals(testList, expectedList);
     }
 
+    /**
+     * Given: An populated List of an odd size.
+     * When: Array is getting sorted.
+     * Then: The List should be sorted correctly.
+     */
     @Test
     @DisplayName("Test {4,3,2,1} Expect {1,2,3,4}")
     public void testPopulatedEvenList(){
@@ -42,6 +57,11 @@ class BubbleSortDeviceConcurTest {
         assertEquals(testList, expectedList);
     }
 
+    /**
+     * Given: An populated List of an even size.
+     * When: Array is getting sorted.
+     * Then: The List should be sorted correctly.
+     */
     @Test
     @DisplayName("Test {5,4,3,2,1} Expect {1,2,3,4,5}")
     public void testPopulatedOddList(){
@@ -51,6 +71,11 @@ class BubbleSortDeviceConcurTest {
         assertEquals(testList, expectedList);
     }
 
+    /**
+     * Given: An populated List with duplications.
+     * When: Array is getting sorted.
+     * Then: The List should be sorted correctly.
+     */
     @Test
     @DisplayName("Test {5,5,3,2,1} Expect {1,2,3,5,5}")
     public void testListPopulatedWithOneDuplicate(){
@@ -60,6 +85,11 @@ class BubbleSortDeviceConcurTest {
         assertEquals(testList, expectedList);
     }
 
+    /**
+     * Given: An populated List only one Number.
+     * When: Array is getting sorted.
+     * Then: The Algorithm returns a List with only this Number.
+     */
     @Test
     @DisplayName("Test {1} Expect {1}")
     public void testListPopulatedWithOneNumberOnly(){
@@ -69,6 +99,11 @@ class BubbleSortDeviceConcurTest {
         assertEquals(testList, expectedList);
     }
 
+    /**
+     * Given: An already sorted List.
+     * When: Array is getting sorted.
+     * Then: The List should remain sorted.
+     */
     @Test
     @DisplayName("Test {1,2,3,4,5} Expect {1,2,3,4,5}")
     public void testAlreadySortedList(){
