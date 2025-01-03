cdb48e2b18ecba6daf0756f033c495779bf2294f
Jan Jungwirth <125290988+janjungwirth@users.noreply.github.com>
2025-01-03 10:32:21 +0100

    Performance Tests Jan

--- src/main/java/at/fh_burgenland/bswe/algo/Main.java
+++ src/main/java/at/fh_burgenland/bswe/algo/Main.java
@@ -9,10 +9,5 @@ public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.show();
-        //TODO:
-        // - Implement Bubble Sort variations
-        // - Implement performance testing
-        // - Run performance testing on different devices
-        // - Write complexity and performance analysis pdf
     }
 }
